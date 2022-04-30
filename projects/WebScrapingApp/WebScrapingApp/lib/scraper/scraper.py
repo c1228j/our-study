cf02698e77070c3bb73ce291424b7e3d178a40b9
virtual-hippo <60084645+virtual-hippo@users.noreply.github.com>
2022-04-30 21:20:54 +0900

    added method that scrape from YahooNews

--- projects/WebScrapingApp/WebScrapingApp/lib/scraper/scraper.py
+++ projects/WebScrapingApp/WebScrapingApp/lib/scraper/scraper.py
@@ -1,35 +1,30 @@
 from lib.scraper.news import News
+import requests
+from bs4 import BeautifulSoup
 
 # ヤフーニュースから何かしらのニュースを取得してくる
 class ScraperFromYahoo:
-    SRC_URL = ''
-    def __init__(self):
-        pass
-
-    def search(self, keywords, newsNum=5):
+    SRC_URL = 'https://news.yahoo.co.jp/search'
+    def search(self, keyword, newsNum=5):
         """
             指定したワードで検索を行い、指定された件数のニュースを返す
             Args:
-                keywords: str : 検索に使用するキーワード
+                keyword: str : 検索に使用するキーワード
                 newsNum:  int : 数値
             Returns:
                 newsのリスト
         """
-        # ToDo: スクレイピングする
+        res = requests.get(ScraperFromYahoo.SRC_URL + "?p=" + keyword + '&ei=utf-8')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        newsFeed_item_elms = soup.find_all('li', {'class': 'newsFeed_item-normal'})
         
         news_list = []
-        for i in range(newsNum):
-            # ToDo: スクレイピングした結果を詰め込む
-            news = News("title" + str(i), "url" + str(i), "body" + str(i))
+        for elem in newsFeed_item_elms[0:newsNum]:
+            title = elem.find('div', {'class': 'newsFeed_item_title'}).get_text()
+            url = elem.find('a').get('href')
+            # ToDo: 本文の取得
+            body = ""
+            news = News(title, url, body)
             news_list.append(news)
-
+        
         return news_list
-
-# 動作確認用
-if __name__ == '__main__':
-    scraper = ScraperFromYahoo()
-    news_list = scraper.search("3", 5)
-    for news in news_list:
-        print(f'title: {news.title}')
-        print(f'url: {news.url}')
-        print(f'body: {news.body}')
