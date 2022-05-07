662f7a6ee72a3000184b68ff18887395a4460f62
c1228j <cen201228.cjk@gmail.com>
2022-05-07 15:53:02 +0900

    sampleアプリにスクレイピングのコードを反映

--- projects/WebScrapingApp/backend/config/settings.py
+++ projects/WebScrapingApp/backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-na$!!kb39+m4+xouub#nmz%cvgkyma_-)yq0)(u-n4k%80nj-n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["backend"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- projects/WebScrapingApp/backend/sample/views.py
+++ projects/WebScrapingApp/backend/sample/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
 from django.views import generic
 
+from lib.scraper import scraper
+
 # sample.htmlを表示するためのビュー
 class SampleView(generic.TemplateView):
     template_name = 'sample/sample.html'
+
+    DEFAULT_KEY_WORD = "ウクライナ"
 
     # テンプレートに表示したいことがある時
     """
         スクレイピングした結果を表示する場合
         context['テンプレートで使用する変数名'] = スクレイピングの結果
-
     """
+
+    def __init__(self):
+        self.scraper = scraper.ScraperFromYahoo()
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        news_list = self.scraper.search(SampleView.DEFAULT_KEY_WORD, 5)
+        data_list = []
+        for news in news_list:
+            data = {"title":news.title, "url":news.url, "body": news.body}
+            data_list.append(data)
+
+
         context['message'] = 'サンプル用のコードを作成しました。'
+        context['news_list'] = data_list
 
         return context
