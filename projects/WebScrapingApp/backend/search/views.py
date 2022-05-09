573d8e83dc130566ff9aa5d30630833d05c15069
c1228j <cen201228.cjk@gmail.com>
2022-05-09 16:08:25 +0900

    ウクライナ用のビューと検索機能があるビューを作成

--- projects/WebScrapingApp/backend/search/views.py
+++ projects/WebScrapingApp/backend/search/views.py
@@ -1,1 +1,53 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+
+from lib.scraper import scraper
+from .forms import SearchForm
+
+class ScrapingUkraineView(generic.TemplateView):
+    template_name = 'search/index.html'
+
+    def __init__(self):
+        self.scraper = scraper.ScraperFromYahoo()
+        self.KEYWORD = 'ウクライナ'
+        self.number = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        news_list = self.scraper.search(self.KEYWORD, self.number)
+        data_list = []
+        for news in news_list:
+            data = {"title":news.title, "url":news.url, "body": news.body}
+            data_list.append(data)
+
+        context['data_list'] = data_list
+        context['keyword'] = self.KEYWORD
+
+        return context
+
+class ScrapingView(generic.FormView):
+    template_name = 'search/index.html'
+    form_class = SearchForm
+    success_url = reverse_lazy('search:index')
+
+    def __init__(self):
+        self.scraper = scraper.ScraperFromYahoo()
+        self.number = 5
+        self.KEYWORD = ''
+
+    def form_valid(self, form):
+        keyword = form.cleaned_data['search']
+        ScrapingUkraineView.KEYWORD = keyword
+
+        news_list = self.scraper.search(ScrapingUkraineView.KEYWORD, self.number)
+        data_list = []
+        for news in news_list:
+            data = {'title': news.title, 'url': news.url}
+            data_list.append(data)
+
+        context = self.get_context_data(data_list=data_list, keyword=ScrapingUkraineView.KEYWORD)
+        return self.render_to_response(context)
+        
+
