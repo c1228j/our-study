538f44b6b2e1298fdde0ba9296972b8a12ff2488
c1228j <cen201228.cjk@gmail.com>
2022-05-09 16:09:03 +0900

    urlを設定しました

--- projects/WebScrapingApp/backend/search/urls.py
+++ projects/WebScrapingApp/backend/search/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'search'
 
 urlpatterns = [
-    
+    path('', views.ScrapingView.as_view(), name='index'),
+    path('ukraine/', views.ScrapingUkraineView.as_view(), name='ukraine'),
 ]
