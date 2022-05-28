d2b7ffed4de7b17d87287bc7ab96ab89f329a9a5
c1228j <cen201228.cjk@gmail.com>
2022-05-28 18:29:45 +0900

    refator: form_validに修正用のコメントを追加

--- projects/WebScrapingApp/backend/search/views.py
+++ projects/WebScrapingApp/backend/search/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 from lib.scraper import scraper
-from .forms import SearchForm
+from .forms import KeywordForm
 
 class ScrapingUkraineView(generic.TemplateView):
     template_name = 'search/index.html'
@@ -29,9 +29,10 @@ class ScrapingUkraineView(generic.TemplateView):
 
 class ScrapingView(generic.FormView):
     template_name = 'search/index.html'
-    form_class = SearchForm
+    form_class = KeywordForm
     success_url = reverse_lazy('search:index')
 
+    # form_validにあるコメントにした場合、次のNEWS_KEYWORDを削除する
     NEWS_KEYWORD = "ウクライナ"
     NEWS_NUMBER = 5
 
@@ -39,16 +40,22 @@ class ScrapingView(generic.FormView):
         self.scraper = scraper.ScraperFromYahoo()
 
     def form_valid(self, form):
+
+        # この2行を削除する
         keyword = form.cleaned_data['keyword']
         ScrapingUkraineView.NEWS_KEYWORD = keyword
 
         news_list = self.scraper.search(ScrapingUkraineView.NEWS_KEYWORD, self.NEWS_NUMBER)
+        # new_list = self.scraper.search(form.cleaned_data['keyword'], self.NEWS_NUMBER) に修正する
+
         data_list = []
         for news in news_list:
             data = {'title': news.title, 'url': news.url}
             data_list.append(data)
 
         context = self.get_context_data(data_list=data_list, keyword=ScrapingUkraineView.NEWS_KEYWORD)
+        # context = self.get_context_data(data_list=data_list, keyword=form.cleaned_data['keyword'])に修正する
+
         return self.render_to_response(context)
         
 
